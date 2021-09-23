d7cbc7a77b7cf4ffdcd705d6eb661a3a707694df
Jason Carr <carrjsn@gmail.com>
2021-09-22 19:49:06 -0700

    render tests for spot and spotlist enzyme

--- test/manageSpots/manageSpots.test.js
+++ test/manageSpots/manageSpots.test.js
@@ -2,10 +2,34 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import ManageSpots from '../../client/src/components/spotManagement/ManageSpots.jsx';
 import TabSelector from '../../client/src/components/shared/tabSelector/TabSelector.jsx';
-import Spot from '../../client/src/components/spotManagement/Spot.jsx'
+import Spot from '../../client/src/components/spotManagement/Spot.jsx';
+import SpotList from '../../client/src/components/spotManagement/SpotList.jsx'
 
 
-test('checks that spot management component is rendering', () => {
-  const wrapper = shallow(<Spot />, {disableLifecycleMethods: true})
-  expect(wrapper.exists('.spot-card')).toBe(true);
+describe('spot component', () => {
+  it('checks that spot component is rendering', () => {
+    const wrapper = shallow(<Spot />, {disableLifecycleMethods: true})
+    expect(wrapper.exists('.spot-card')).toBe(true);
+  });
 });
+
+describe('spotlist component', () => {
+  const spots = [
+    {
+      spot_id: 1,
+      price: 1,
+      photo_url: '',
+      address: ''
+    }
+  ]
+  it('checks that spotlist component is rendering', () => {
+    const wrapper = shallow(<SpotList spots={spots}/>, {disableLifecycleMethods: true})
+    expect(wrapper.exists('.spot-list')).toBe(true);
+  });
+});
+
+
+
+
+
+
