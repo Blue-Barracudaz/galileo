8826a0573c78c520d36c216e9e9dfca935ad8704
Jason Carr <carrjsn@gmail.com>
2021-10-05 18:21:37 -0700

    update s3 access, remove comments

--- server/routes/controllers/manageSpots.js
+++ server/routes/controllers/manageSpots.js
@@ -3,33 +3,29 @@ const fs = require('fs');
 const AWS = require('aws-sdk');
 const multiparty = require('multiparty');
 
-const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-});
+const s3 = new AWS.S3({apiVersion: '2006-03-01'});
 
 const getMySpots = (req, res) => {
- // req.query.id = hostid
  model.getSpots(req.query.id)
    .then((spots) => {
-    //  console.log('getting spots back to controller', spots);
      res.status(200);
      res.send(spots)
    })
    .catch((err) => {
      console.log('error getting spots from models');
+     res.sendStatus(404)
    })
 }
 
 const addNewSpot = (req, res) => {
- // req.body = { hostId:   , lat:    , long:     , price:    , address:   , type:    , photo:    , }
+ // req.body = { hostId:   , lat:    , long:     , price:    , address:   , type:    , photo:    }
  model.addSpot(req.body)
    .then(() => {
-    //  console.log('success adding new post - controller');
      res.sendStatus(201);
    })
    .catch((err) => {
      console.log('error posting new spot - controller', err);
+     res.sendStatus(404)
    })
 }
 
@@ -37,12 +33,12 @@ const getSpotDetails = (req, res) => {
   // req.query.id = spotid
   model.getInfo(req.query.id)
    .then((info) => {
-    //  console.log('getting spots details to controller', info);
      res.status(200);
      res.send(info);
    })
    .catch((err) => {
      console.log('error getting spot details from models', err);
+     res.sendStatus(404)
    })
 }
 
@@ -50,17 +46,16 @@ const updateSpotDetails = (req, res) => {
   // req.body = { photo:   , type:    , spotId:     , price:    }
   model.updateInfo(req.body)
     .then(() => {
-      // console.log('success updating spot details - controller');
       res.sendStatus(204);
     })
     .catch((err) => {
       console.log('error updating spot details - controller', err);
+      res.sendStatus(404);
     })
 
 }
 
 const uploadImage = (req, res) => {
-  // upload to s3
   const form = new multiparty.Form();
 
   form.on('part', function(part) {
@@ -74,11 +69,8 @@ const uploadImage = (req, res) => {
     s3.upload(params, (err, data) => {
       if (err) {
         console.log('error adding img to s3', err);
-        res.sendStatus(500)
+        res.sendStatus(500);
       } else {
-
-        console.log('success adding image', data)
-        // get url
         res.status(201);
         res.send(data.Location);
       }
